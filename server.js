932ac6079b09dad18598d80c6ffdf93daa8e7bf6
MyStic <khanh.votuan@hcmut.edu.vn>
2023-05-04 20:44:53 +0700

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({ path: './.env' });
+const DB = process.env.DB.replace('<password>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connect successfully');
+  });
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server starting at port ${PORT}`);
+});
