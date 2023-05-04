da13669d101552904e1800371b14268055756b24
MyStic <khanh.votuan@hcmut.edu.vn>
2023-05-04 21:25:00 +0700

    category, product model

---
+++ models/category.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+  },
+  icon: {
+    type: String,
+  },
+
+  image: {
+    type: String,
+  },
+});
+
+exports.Category = mongoose.model('Category', CategorySchema);
