03cbafc2800f5372e5ca7eefc49295412c9a813a
MyStic <khanh.votuan@hcmut.edu.vn>
2023-05-22 21:59:13 +0700

    sign up feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,22 @@
+const { User } = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signIn = catchAsync(async (req, res, next) => {});
 
 exports.signOut = catchAsync(async (req, res, next) => {});
 
-exports.signUp = catchAsync(async (req, res, next) => {});
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      newUser,
+    },
+  });
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -10,6 +11,17 @@ const userSchema = mongoose.Schema({
     required: true,
   },
   password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (password) {
+        return password === this.passwordConfirm;
+      },
+      message: () => 'Password and password confirm not match',
+    },
+    select: false,
+  },
+  passwordConfirm: {
     type: String,
     required: true,
   },
@@ -35,12 +47,20 @@ const userSchema = mongoose.Schema({
   },
   phone: {
     type: String,
-    required: true,
+    default: '',
   },
   isAdmin: {
     type: Boolean,
     default: false,
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 exports.User = mongoose.model('User', userSchema);
